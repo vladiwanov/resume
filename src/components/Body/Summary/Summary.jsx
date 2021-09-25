e54b6b62d584cce20507709938745f5bbb49c00c
vladiwanov <3298901@gmail.com>
2021-09-25 18:59:47 +0300

    fixed styles

--- src/components/Body/Summary/Summary.jsx
+++ src/components/Body/Summary/Summary.jsx
@@ -4,7 +4,7 @@ import summary from '../../../db/summary.json'
 export default function Objective() {
   return (
     <div className={`${s.content} ${s.objective}`}>
-      <h2 className={`${s.title} ${s.objectiveTitle}`}>Objective:</h2>
+      <h2 className={`${s.title} ${s.objectiveTitle}`}>Summary:</h2>
       <p className={`${s.item} ${s.objectiveItem}`}>
         {summary}
       </p>
