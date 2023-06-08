c3f9d008e7b1f102ac4ae81c059d09a3425fe18f
vladiwanov <3298901@gmail.com>
2023-06-08 20:49:27 +0300

    added ITC personal project

--- src/db/projects-db/personal-projects.js
+++ src/db/projects-db/personal-projects.js
@@ -1,24 +1,31 @@
 const personalProjects = [
   {
+    Project: 'ITC',
+    link: 'https://ivanov-tech-consult.netlify.app/',
+    aboutProject: 'Web page of Engineering Direction Consulting',
+    usedTechnologies: ['React.js', 'TypeScript', 'SASS', 'Grid'],
+    id: '1',
+  },
+  {
     Project: 'IV-Engineering frontend',
     link: 'https://iv-en.com/',
     aboutProject: 'Web page  of Engineering company',
     usedTechnologies: ['React.js', 'SASS', 'Grid', 'AJAX'],
-    id: '1',
+    id: '2',
   },
   {
     Project: 'Retro Project',
     link: 'https://retroproject.com.ua/',
     aboutProject: 'Web page of architectural design company',
     usedTechnologies: ['React.js', 'SASS', 'Grid'],
-    id: '2',
+    id: '3',
   },
   {
     Project: 'Resume Volodymyr Ivanov',
     link: 'https://vladiwanov.netlify.app/',
     aboutProject: 'Junior Frontend Developer Resume',
     usedTechnologies: ['React.js', 'TypeScript', 'SASS', 'Grid'],
-    id: '3',
+    id: '4',
   },
 ];
 
